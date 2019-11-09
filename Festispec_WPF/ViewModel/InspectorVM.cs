6aa1290fcac96c8f7bad4dffe5985e9821fadee6
Zakariakarboub <49561317+Zakariakarboub@users.noreply.github.com>
2019-11-09 22:12:39 +0100

    Tussentijdse commit
    InspectorVM gedaan, databinding moet nog recht gezet worden

--- Festispec_WPF/ViewModel/InspectorVM.cs
+++ Festispec_WPF/ViewModel/InspectorVM.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GalaSoft.MvvmLight;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,33 +7,67 @@ using System.Threading.Tasks;
 
 namespace Festispec_WPF.ViewModel
 {
-    class InspectorVM
+    class InspectorVM : ViewModelBase
     {
-        public InspectorVM {
-    }
-    private Inspecteur _Inspecteur;
-
-    public int Inspector_ID 
+        public InspectorVM()
         {
-        get { return _Inspecteur.Inspecteur_ID }
-        set { _Inspecteur = value; RaisePropertyChanged("Inspecteur_Id"); }
+            this._inspecteur = new NAW_inspecteur();
         }
 
-    public int Voornaam
-    {
-        get { return _Inspecteur.Voornaam}
-        set { _Inspecteur = value; RaisePropertyChanged("Voornaam"); }
-    }
+        internal NAW_inspecteur ToModel()
+        {
+            return _inspecteur;
+        }
 
-    public int Achternaam
-    {
-        get { return _Inspecteur.Inspecteur_ID }
-        set { _Inspecteur = value; RaisePropertyChanged("Tussenvoegsel"); }
-    }
-    public int Achternaam
-    {
-        get { return _Inspecteur.Inspecteur_ID }
-        set { _Inspecteur = value; RaisePropertyChanged("Tussenvoegsel"); }
-    }
+        private NAW_inspecteur _inspecteur;
 
+        public string ID
+        {
+            get { return _inspecteur.ID; }
+            set { _inspecteur.ID = value; RaisePropertyChanged("Id"); }
+        }
+
+        public string Voornaam
+        {
+            get { return _inspecteur.Voornaam; }
+            set { _inspecteur.Voornaam = value; RaisePropertyChanged("Voornaam"); }
+        }
+
+        public string Tussenvoegsel
+        {
+            get { return _inspecteur.Tussenvoegsel; }
+            set { _inspecteur.Tussenvoegsel = value; RaisePropertyChanged("Tussenvoegsel"); }
+        }
+
+        public string Achternaam
+        {
+            get { return _inspecteur.Achternaam; }
+            set { _inspecteur.Achternaam = value; RaisePropertyChanged("Achternaam"); }
+        }
+        public string Postcode
+        {
+            get { return _inspecteur.Postcode; }
+            set { _inspecteur.Postcode = value; RaisePropertyChanged("Postcode"); }
+        }
+        public string Huisnummer
+        {
+            get { return _inspecteur.Huisnummer; }
+            set { _inspecteur.Huisnummer = value; RaisePropertyChanged("Huisnummer"); }
+        }
+        public DateTime Geboortedatum
+        {
+            get { return _inspecteur.Geboortedatum; }
+            set { _inspecteur.Geboortedatum = value; RaisePropertyChanged("Geboortedatum"); }
+        }
+        public string IBAN
+        {
+            get { return _inspecteur.IBAN; }
+            set { _inspecteur.IBAN = value; RaisePropertyChanged("IBAN"); }
+        }
+        public string Email
+        {
+            get { return _inspecteur.Email; }
+            set { _inspecteur.Email = value; RaisePropertyChanged("Email"); }
+        }
+    }
 }
