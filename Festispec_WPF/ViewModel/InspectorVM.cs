651c073b8d1b8b69f964510e09825cdacf3987c2
Zakariakarboub <49561317+Zakariakarboub@users.noreply.github.com>
2019-11-09 20:53:52 +0100

    Tussentijdense commit

---
+++ Festispec_WPF/ViewModel/InspectorVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festispec_WPF.ViewModel
+{
+    class InspectorVM
+    {
+        public InspectorVM {
+    }
+    private Inspecteur _Inspecteur;
+
+    public int Inspector_ID 
+        {
+        get { return _Inspecteur.Inspecteur_ID }
+        set { _Inspecteur = value; RaisePropertyChanged("Inspecteur_Id"); }
+        }
+
+    public int Voornaam
+    {
+        get { return _Inspecteur.Voornaam}
+        set { _Inspecteur = value; RaisePropertyChanged("Voornaam"); }
+    }
+
+    public int Achternaam
+    {
+        get { return _Inspecteur.Inspecteur_ID }
+        set { _Inspecteur = value; RaisePropertyChanged("Tussenvoegsel"); }
+    }
+    public int Achternaam
+    {
+        get { return _Inspecteur.Inspecteur_ID }
+        set { _Inspecteur = value; RaisePropertyChanged("Tussenvoegsel"); }
+    }
+
+}
