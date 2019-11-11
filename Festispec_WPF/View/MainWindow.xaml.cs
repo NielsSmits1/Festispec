cefd049bcaaf4d1383ae6342cd0e734484a0f02b
Niels <nga.smits@student.avans.nl>
2019-11-11 12:12:03 +0100

    Merge fixes

--- Festispec_WPF/View/MainWindow.xaml.cs
+++ Festispec_WPF/View/MainWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Festispec_WPF
+namespace Festispec_WPF.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class InspectorCrudWindow : Window
+    public partial class MainWindow : Window
     {
-        public InspectorCrudWindow()
+        public MainWindow()
         {
             InitializeComponent();
         }
