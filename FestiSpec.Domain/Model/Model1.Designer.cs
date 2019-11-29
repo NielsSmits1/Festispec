86bad6c7ab5e83c15d734e85ee8dc9c2c7d7e4a7
David <davidvdzalm@gmail.nl>
2019-11-29 12:12:05 +0100

    updated nuget packages

--- FestiSpec.Domain/Model/Model1.Designer.cs
+++ FestiSpec.Domain/Model/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\NGAPA\Documents\GitHub\Festispec\FestiSpec.Domain\Model\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Avans\Leerjaar 2\Blok 6\project\Festispec\FestiSpec.Domain\Model\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
