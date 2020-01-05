825711bc52d62cbf46f842d944437326e9595331
dirkteulings <dteulings.dt@gmail.com>
2020-01-05 16:44:52 +0100

    now able to semi generate from database

--- Festispec_WPF/ViewModel/RapportageVM.cs
+++ Festispec_WPF/ViewModel/RapportageVM.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 using Festispec_WPF.Helpers;
 using Festispec_WPF.Model.UnitOfWork;
@@ -43,13 +44,77 @@ namespace Festispec_WPF.ViewModel
         {
             UOW = ViewModelLocator.UOW;
             GenerateCommand = new RelayCommand(GeneratePdf);
+
+            var id = UOW.Inspections.Get(11).Vragenlijst.First().ID;
+            var inspection = UOW.Inspections.Get(11).Inspectie_Wel_Ingevuld_Vragenlijst.Where(q=> q.Vragenlijst.Stamt_af_van_ID == id).ToList();
+            inspection.Add(inspection.First());
+            var vragen = new List<Question>();
+
+            var first = true;
+            foreach (var ins in inspection)
+            {
+                if (first)
+                {
+                    ins.Vragenlijst.Openvraag_vragenlijst.ToList().ForEach(a =>
+                    {
+                        vragen.Add(new Question()
+                        {
+                            Id = a.Openvraag_ID,
+                            QuestionTitle = a.Openvraag.Vraag,
+                            GivenAwnsers = new List<string>() { a.Antwoord }
+                        });
+                    });
+
+                    ins.Vragenlijst.Meerkeuzevraag_vragenlijst.ToList().ForEach(a =>
+                    {
+                        vragen.Add(new Question()
+                        {
+                            Id = a.Meerkeuzevraag_ID,
+                            QuestionTitle = a.Meerkeuzevraag.Vraag,
+                            GivenAwnsers = new List<string>() { a.Antwoord }
+                        });
+                    });
+
+                    ins.Vragenlijst.Bijlagevraag_vragenlijst.ToList().ForEach(a =>
+                    {
+                        vragen.Add(new Question()
+                        {
+                            Id = a.Bijlagevraag_ID,
+                            QuestionTitle = a.Bijlagevraag.Vraag,
+                            GivenAwnsers = new List<string>() { "data:image/png;base64," + Convert.ToBase64String(a.FileBytes) },
+                            Type = "img"
+                        });
+                    });
+                }
+                else
+                {
+                    ins.Vragenlijst.Openvraag_vragenlijst.ToList().ForEach(a =>
+                    {
+                        vragen.Where(q => q.QuestionTitle == a.Openvraag.Vraag).First().GivenAwnsers.Add(a.Antwoord);
+                    });
+
+                    ins.Vragenlijst.Meerkeuzevraag_vragenlijst.ToList().ForEach(a =>
+                    {
+                        vragen.Where(q => q.QuestionTitle == a.Meerkeuzevraag.Vraag).First().GivenAwnsers.Add(a.Antwoord);
+                    });
+
+                    ins.Vragenlijst.Bijlagevraag_vragenlijst.ToList().ForEach(a =>
+                    {
+                        vragen.Where(q => q.QuestionTitle == a.Bijlagevraag.Vraag).First().GivenAwnsers.Add("data:image/png;base64," + Convert.ToBase64String(a.FileBytes));
+                    });
+                }
+
+                first = false;
+            }
+
             Charts = new ObservableCollection<string>()
             {
+                "",
                 "Bar",
                 "Row",
                 "Pie"
             };
-            
+
             selectedInspection = new RapportageInfo()
             {
                 Advice = "Meer bier drinken de man, ja en bier ja." +
@@ -81,42 +146,7 @@ namespace Festispec_WPF.ViewModel
                 InspectionTitle = "Is er genoeg bier op het festival?",
                 Introduction = "Wij Gaan kijken of er genoeg bier is.",
                 SummaryOfInspection = "Er was niet genoeg bier de man.",
-                Questions = new List<Question>()
-                {
-                    new Question()
-                    {
-                        Id = 1,
-                        GivenAwnsers = new List<string>()
-                        {
-                            "A",
-                            "A",
-                            "B"
-                        },
-                        QuestionTitle = "Hoeveel bier was er?"
-                    },
-                    new Question()
-                    {
-                        Id = 2,
-                        GivenAwnsers = new List<string>()
-                        {
-                            "A",
-                            "B",
-                            "C"
-                        },
-                        QuestionTitle = "Hoeveel bier was er verspilt?"
-                    },
-                    new Question()
-                    {
-                        Id = 3,
-                        GivenAwnsers = new List<string>()
-                        {
-                            "A",
-                            "B",
-                            "C"
-                        },
-                        QuestionTitle = "Hoeveel bier had je op?"
-                    }
-                }
+                Questions = vragen
             };
 
             questions = new ObservableCollection<Question>(selectedInspection.Questions);
