ad103672e40b7d1cf3149894563104b255d9ffcc
Niels <nga.smits@student.avans.nl>
2020-01-10 00:04:58 +0100

    Pussshhhh

--- FestiSpec.Domain/Model/Contactpersoon.cs
+++ FestiSpec.Domain/Model/Contactpersoon.cs
@@ -11,7 +11,8 @@ namespace FestiSpec.Domain.Model
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Contactpersoon
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,7 +20,8 @@ namespace FestiSpec.Domain.Model
         {
             this.Afspraak = new HashSet<Afspraak>();
         }
-    
+
+        
         public int ID { get; set; }
         public int Klant_ID { get; set; }
         public string Voornaam { get; set; }
