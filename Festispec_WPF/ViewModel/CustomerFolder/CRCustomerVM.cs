adce5e166858fde389890c64ec5eee6ab2b8e862
Kenneth Gerrits <kennethgerrits@gmail.com>
2020-01-14 14:31:13 +0100

    schone was

--- Festispec_WPF/ViewModel/CustomerFolder/CRCustomerVM.cs
+++ Festispec_WPF/ViewModel/CustomerFolder/CRCustomerVM.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Forms;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight;
 using Festispec_WPF.View;
 using Festispec_WPF.Model.UnitOfWork;
-using FestiSpec.Domain.Model;
-using System.Windows;
+
 using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace Festispec_WPF.ViewModel
--- Festispec_WPF/ViewModel/CustomerFolder/CustomerVM.cs
+++ Festispec_WPF/ViewModel/CustomerFolder/CustomerVM.cs
@@ -1,30 +1,21 @@
 
 using GalaSoft.MvvmLight;
 ﻿using FestiSpec.Domain.Model;
-using Festispec_WPF.Model;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Festispec_WPF.ViewModel
 {
     public class CustomerVM : ViewModelBase
     {
         private Klant _customer;
-
         private NAW_KlantVM _NAW_Klant;
-
         private NAW_KlantVM _NAW_Customer;
         private ObservableCollection<ContactPersonVM> _ContactPersons;
-
-
+        
         public NAW_KlantVM NAW_Klant
         {
-            get { return _NAW_Klant; }
-            set { _NAW_Klant = value; }
+            get => _NAW_Klant;
+            set => _NAW_Klant = value;
         }
 
         public int ID
@@ -38,13 +29,13 @@ namespace Festispec_WPF.ViewModel
         }
         public ObservableCollection<ContactPersonVM> ContactPersons
         {
-            get { return _ContactPersons; }
-            set { _ContactPersons = value; }
+            get => _ContactPersons;
+            set => _ContactPersons = value;
         }
         public string CompanyName
         {
-            get { return _customer.Bedrijfsnaam; }
-            set { _customer.Bedrijfsnaam = value; }
+            get => _customer.Bedrijfsnaam;
+            set => _customer.Bedrijfsnaam = value;
         }
         public CustomerVM(Klant customer)
         {
@@ -54,24 +45,13 @@ namespace Festispec_WPF.ViewModel
         public CustomerVM()
         {
             _customer = new Klant();
-
             _NAW_Klant = new NAW_KlantVM();
-
             _NAW_Customer = new NAW_KlantVM();
         }
         public Klant CustomerData
         {
-            get
-            {
-                return _customer;
-            }
-            set
-            {
-                _customer = value;
-            }
-
+            get => _customer;
+            set => _customer = value;
         }
-
-
     }
 }
