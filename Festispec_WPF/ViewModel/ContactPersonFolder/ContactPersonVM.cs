bf2de728f4fa7b03ac8b641e2a43121b685c0356
Kenneth Gerrits <kennethgerrits@gmail.com>
2020-01-14 14:26:07 +0100

    cleanup

--- Festispec_WPF/ViewModel/ContactPersonFolder/ContactPersonVM.cs
+++ Festispec_WPF/ViewModel/ContactPersonFolder/ContactPersonVM.cs
@@ -1,10 +1,4 @@
 ﻿using FestiSpec.Domain.Model;
-using Festispec_WPF.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Festispec_WPF.ViewModel
 {
@@ -14,33 +8,33 @@ namespace Festispec_WPF.ViewModel
         private CustomerVM _customer;
         public string FirstName
         {
-            get { return _contactPerson.Voornaam; }
-            set { _contactPerson.Voornaam = value; }
+            get => _contactPerson.Voornaam;
+            set => _contactPerson.Voornaam = value;
         }
         public string Affix
         {
-            get { return _contactPerson.Tussenvoegsel; }
-            set { _contactPerson.Tussenvoegsel = value; }
+            get => _contactPerson.Tussenvoegsel;
+            set => _contactPerson.Tussenvoegsel = value;
         }
         public string LastName
         {
-            get { return _contactPerson.Achternaam; }
-            set { _contactPerson.Achternaam = value; }
+            get => _contactPerson.Achternaam;
+            set => _contactPerson.Achternaam = value;
         }
         public string PhoneNumber
         {
-            get { return _contactPerson.Telefoonnummer; }
-            set { _contactPerson.Telefoonnummer = value; }
+            get => _contactPerson.Telefoonnummer;
+            set => _contactPerson.Telefoonnummer = value;
         }
         public string Email
         {
-            get { return _contactPerson.Email; }
-            set { _contactPerson.Email = value; }
+            get => _contactPerson.Email;
+            set => _contactPerson.Email = value;
         }
         public CustomerVM customer
         {
-            get { return _customer; }
-            set { _customer = value; }
+            get => _customer;
+            set => _customer = value;
         }
         public ContactPersonVM(Contactpersoon cp)
         {
@@ -53,18 +47,10 @@ namespace Festispec_WPF.ViewModel
             _customer = new CustomerVM();
         }
 
-
         public Contactpersoon ContactPersonData
         {
-            get
-            {
-                return _contactPerson;
-            }
-            set
-            {
-                _contactPerson = value;
-            }
+            get => _contactPerson;
+            set => _contactPerson = value;
         }
-
     }
 }
